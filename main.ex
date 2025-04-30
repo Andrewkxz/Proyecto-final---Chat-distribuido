c84905ce8b2c225806147dd56281abd20ccb387c
Andrewkxz <141884726+Andrewkxz@users.noreply.github.com>
2025-04-30 11:34:41 -0500

    por fin

--- main.ex
+++ main.ex
@@ -2,4 +2,5 @@ defmodule Main do
   IO.puts("Hello, world!")
   IO.puts("This is a simple Elixir program that prints a message to the console.")
 end
-buenastardes noches lo que sea 
+buenastardes noches lo que sea
+xfin pude fucking iniciar el repositorio para hacer cambios desde visual, que bendicion, diosmio
